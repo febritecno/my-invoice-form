bb36e0c5dbdb338d874088501b0299e1c38b7fe9
clonez9494 <clonez9494@gmail.com>
2024-05-12 05:40:06 +0700

    chore: update package.json

--- frontend/src/context/context.js
+++ frontend/src/context/context.js
@@ -1,11 +1,9 @@
 import { createContext, useReducer, useState, useEffect } from 'react';
 import { invoicesReducers } from '../store/reducers/invoicesReducers';
 import generatePaymentDueDate from '../utilities/generatePaymentDueDate';
-import generateID from '../utilities/generateId';
+import generateID from '../utilities/generateId'; require('dotenv').config();
 
-require('dotenv').config();
-
-const BASE_API_URL = process.env.BASE_API_URL || "http://localhost:5000";
+const BASE_API_URL = "https://invoices-928j-8fuwboslj-febritecnos-projects.vercel.app";
 const API_URL = `${BASE_API_URL}/api`;
 
 const getInvoicesFromAPI = async () => {
